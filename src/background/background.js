acd930d422199889170f6ae66aa31aa8536864fa
Gabriel Morales <gabrielmorales1@protonmail.com>
2024-08-19 00:33:57 -0500

    Split the background.js file into separate files by actions

--- src/background/background.js
+++ src/background/background.js
@@ -1,32 +1,32 @@
-import { Prompts } from "./OpenAI/prompts/index.js";
+import handleEditableText from "./editable.js";
 
 chrome.runtime.onInstalled.addListener(async () => {
-  await chrome.contextMenus.create({
+  chrome.contextMenus.create({
     id: "spellingAndGrammar",
     title: "Spelling and Grammar",
     type: "normal",
     contexts: ["editable"],
   });
-  await chrome.contextMenus.create({
+  chrome.contextMenus.create({
     id: "summary",
     title: "Summary",
     type: "normal",
-    contexts: ["editable"],
+    contexts: ["selection", "editable"],
   });
-  await chrome.contextMenus.create({
+  chrome.contextMenus.create({
     id: "translate",
     title: "Translate",
     type: "normal",
     contexts: ["editable"],
   });
-  await chrome.contextMenus.create({
+  chrome.contextMenus.create({
     id: "translateEnToEs",
     parentId: "translate",
     title: "English to Spanish",
     type: "normal",
     contexts: ["editable"],
   });
-  await chrome.contextMenus.create({
+  chrome.contextMenus.create({
     id: "translateEsToEN",
     parentId: "translate",
     title: "Spanish to English",
@@ -36,44 +36,7 @@ chrome.runtime.onInstalled.addListener(async () => {
 });
 
 chrome.contextMenus.onClicked.addListener((contextMenuItem, tab) => {
-  chrome.tabs.sendMessage(
-    tab.id,
-    { type: "StartProcessing" },
-    { frameId: contextMenuItem.frameId },
-    (hash) => {
-      const promptID = contextMenuItem.menuItemId;
-      const userMessage = contextMenuItem.selectionText;
-
-      if (!userMessage) {
-        console.error("No hay mensaje del usuario...");
-        return null;
-      }
-
-      const prompt = Prompts[promptID];
-      if (typeof prompt !== "function") {
-        console.error("No se encontró la acción...");
-        return null;
-      }
-
-      return prompt(userMessage)
-        .then((response) => showResult(contextMenuItem, tab, hash, response))
-        .catch(() => console.error("Error processing the action"));
-    },
-  );
-});
-
-function showResult(contextMenuItem, tab, hash, response) {
-  if (!(response && response.choices && response.choices.length > 0)) {
-    return null;
+  if (contextMenuItem.editable) {
+    return handleEditableText(contextMenuItem, tab);
   }
-
-  chrome.tabs.sendMessage(
-    tab.id,
-    {
-      type: "ReplaceText",
-      text: response.choices[0].message.content,
-      hash: hash,
-    },
-    { frameId: contextMenuItem.frameId },
-  );
-}
+});
